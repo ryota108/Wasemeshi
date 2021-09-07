e41e3dc453ca823e8d8b1b68fdc036a03cb4ba25
ryota108 <83957178+ryota108@users.noreply.github.com>
2021-09-07 10:12:22 +0900

    Update wasemeshi.js
    表示件数が増えたことに伴い、処理の追加をおこなった

--- wasemeshi.js
+++ wasemeshi.js
@@ -47,8 +47,67 @@ const modalSecond =new Modal("open2","close2","modal2","mask2")
 modalSecond.toggleOpen()
 modalSecond.toggleClose()
 
-
 const modalThird =new Modal("open3","close3","modal3","mask3")
 modalThird.toggleOpen()
 modalThird.toggleClose()
+
+const modalForth =new Modal("open4","close4","modal4","mask4")
+modalForth.toggleOpen()
+modalForth.toggleClose()
+
+const modalFifth =new Modal("open5","close5","modal5","mask5")
+modalFifth.toggleOpen()
+modalFifth.toggleClose()
+
+const modalSixth=new Modal("open6","close6","modal6","mask6")
+modalSixth.toggleOpen()
+modalSixth.toggleClose()
+
+const modalSeventh =new Modal("open7","close7","modal7","mask7")
+modalSeventh.toggleOpen()
+modalSeventh.toggleClose()
+
+const modalEighth =new Modal("open8","close8","modal8","mask8")
+modalEighth.toggleOpen()
+modalEighth.toggleClose()
+
+const modalNinth =new Modal("open9","close9","modal9","mask9")
+modalNinth.toggleOpen()
+modalNinth.toggleClose()
+
+const modalTenth =new Modal("open10","close10","modal10","mask10")
+modalTenth.toggleOpen()
+modalTenth.toggleClose()
+
+const modal11 =new Modal("open11","close11","modal11","mask11")
+modal11.toggleOpen()
+modal11.toggleClose()
+
+const modal12 =new Modal("open12","close12","modal12","mask12")
+modal12.toggleOpen()
+modal12.toggleClose()
+
+const modal13 =new Modal("open13","close13","modal13","mask13")
+modal13.toggleOpen()
+modal13.toggleClose()
+
+const modal14 =new Modal("open14","close14","modal14","mask14")
+modal14.toggleOpen()
+modal14.toggleClose()
+
+const modal15 =new Modal("open15","close15","modal15","mask15")
+modal15.toggleOpen()
+modal15.toggleClose()
+
+const modal16 =new Modal("open16","close16","modal16","mask16")
+modal16.toggleOpen()
+modal16.toggleClose()
+
+const modal17 =new Modal("open17","close17","modal17","mask17")
+modal17.toggleOpen()
+modal17.toggleClose()
+
+const modal18 =new Modal("open18","close18","modal18","mask18")
+modal18.toggleOpen()
+modal18.toggleClose()
 }
